7544a573b456d882c45c10f7b90a585f9fd4c018
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-18 04:09:17 -0500

    edit

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,12 @@
-const button = document.querySelector('button');
+// $('.dropdown-toggle').dropdown()
 
-button.addEventListener('click', event => {
-  button.innerHTML = `${event.MouseEvent("button")}`;
-});
-$("button").click(function() {
-    $(`button`).append(`<button> LinkedIn Profile </button>`);
-    $(`button`).append(`<button> Another Button </button>`);
-    $(`button`).append(`<button> Something else </button>`);
-});
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', event => {
+//   button.innerHTML = `${event.MouseEvent("button")}`;
+// });
+// $("button").click(function() {
+//     $(`button`).append(`<button> LinkedIn Profile </button>`);
+//     $(`button`).append(`<button> Another Button </button>`);
+//     $(`button`).append(`<button> Something else </button>`);
+// });
